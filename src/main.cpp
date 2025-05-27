532a55e834ee0f11eccfee8c7175a3130fbc3927
Moritz <mowe03@gmail.com>
2025-05-27 17:24:51 +0200

    add fetch content to cmake file.

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,15 @@
 #include "loatconfig.hpp"
 #include "loatsolver.hpp"
 #include "loatintexpr.hpp"
+#include "loatlocation.hpp"
 #include <any>
 #include <stdexcept>
 
 int main()
 {
-    LoatConfig::InitialConfig initialConfig1(
-        LoatConfig::InitialConfig::Engine::ADCL,
-        LoatConfig::InitialConfig::Mode::Safety,
-        LoatConfig::InitialConfig::SmtSolver::Z3,
-        LoatConfig::InitialConfig::Direction::Forward,
-        LoatConfig::InitialConfig::MbpKind::IntMbp,
-        false);
-
-    LoatConfig config1(initialConfig1);
-
-    LoatSolver solver1(config1);
-
+    LoatSolver solver1 = LoatSolver::forSafety();
     solver1.setParameter(DynamicParameterKey::Log, true);
+    solver1.setParameter(DynamicParameterKey::LogPreproc, true);
 
     LoatIntExprPtr five = LoatIntExpression::mkConst(Rational(5));
     LoatIntExprPtr x = LoatIntExpression::mkPreVar("x");
@@ -28,6 +19,32 @@ int main()
     LoatIntExprPtr fivexplusten = (five * x) + ten;
 
     std::cout << fivexplusten << std::endl;
+
+    LoatTransition transition1(
+        LoatLocation("start"),
+        LoatLocation("end"),
+        fivexplusten == LoatIntExpression::mkConst(Rational(15)));
+
+    std::cout << transition1 << std::endl;
+
+    solver1.add(transition1);
+    solver1.setStartLocation(LoatLocation("start"));
+    solver1.addSinkLocation(LoatLocation("end"));
+
+    LoatResult result = solver1.check();
+
+    if (result == LoatResult::SAT)
+    {
+        std::cout << "SAT." << std::endl;
+    }
+    else if (result == LoatResult::UNSAT)
+    {
+        std::cout << "UNSAT" << std::endl;
+    }
+    else
+    {
+        std::cout << "UNKNOWN" << std::endl;
+    }
 
     return 0;
 }
